017078edf69a75d3439a2325306a3c1d86e0bd42
Sam Ko <sumsxing@qq.com>
2016-11-13 22:09:34 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,13 @@
-script
+#! /bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+#=================================================================#
+#   System Required:  CentOS, Debian, Ubuntu                      #
+#   Description: Install haproxy for Shadowsocks server           #
+#   Author: Teddysun <i@teddysun.com>                             #
+#   Intro:  https://shadowsocks.be/10.html                        #
+#=================================================================#
+
+clear
+each "Hello World"
+exit 0
