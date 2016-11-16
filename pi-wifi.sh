7103967afa977c6e98167e97029c8ef74c4f7bc8
Sam Ko <sumsxing@qq.com>
2016-11-16 17:24:26 +0800

    Update pi-wifi.sh

--- pi-wifi.sh
+++ pi-wifi.sh
@@ -6,8 +6,10 @@ export PATH
 #   Description: It's a sh for enable wifi on a centos Pi.        #
 #   Author: sumsxing <sumsxing@qq.com>                            #
 #=================================================================#
+read -p "SSID:" kSSID
+read -p "Password:" kPWD
 sudo yum -y install crda iw wireless
 sudo touch /.rootfs-repartition
 curl --location https://github.com/RPi-Distro/firmware-nonfree/raw/54bab3d6a6d43239c71d26464e6e10e5067ffea7/brcm80211/brcm/brcmfmac43430-sdio.bin > /usr/lib/firmware/brcm/brcmfmac43430-sdio.bin
 curl --location https://github.com/RPi-Distro/firmware-nonfree/raw/54bab3d6a6d43239c71d26464e6e10e5067ffea7/brcm80211/brcm/brcmfmac43430-sdio.txt > /usr/lib/firmware/brcm/brcmfmac43430-sdio.txt
-nmcli d wifi connect <your wifi SSID> password <your wifi password>
+nmcli d wifi connect "$kSSID" password "$kPWD"
